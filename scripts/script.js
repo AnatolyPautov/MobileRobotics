2283bc8be108cdc162df2581fb14775d7ec75e1b
AnatolyPautov <anatolypautov@mail.ru>
2021-09-28 19:25:29 +0600

    fix scripts

--- scripts/script.js
+++ scripts/script.js
@@ -17,25 +17,6 @@ const bodyLock = document.querySelector('body');
     });
 }());
 
-/*-----Modal-Contestants -----*/
-
- (function () {
-    const contestants = document.querySelectorAll('.contestant');
-    const modal = document.querySelector('.modal-contestants');
-    const close = document.getElementById('close-contestants');
-
-    contestants.forEach((contestant) => {
-        contestant.addEventListener('click', () => {
-            modal.classList.toggle('active');
-            bodyLock.classList.toggle('lock');
-        });
-    })
-    close.addEventListener('click', () => {
-        modal.classList.toggle('active');
-        bodyLock.classList.toggle('lock');
-    });
-}());
-
 /*accordion*/  
 
 const cityBlock = document.getElementsByClassName('city__item');
